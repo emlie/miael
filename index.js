83fd8580bf8db6ce6dca3b861486052b86aabb42
emlie <mi-ya@live.no>
2020-02-01 16:07:51 +0100

    vue works

---
+++ index.js
@@ -0,0 +1,40 @@
+// jshint esversion: 6
+
+let elHeader = document.querySelector('#header');
+let elFaceImg = document.querySelectorAll('.faceImg')[0];
+
+const compHeaderHeight = window.getComputedStyle(elHeader).height;
+const headerHeight = pxToNum(compHeaderHeight);
+
+const face = new Vue ({
+  el: '#face',
+  data: {
+    img: 'img/profile.jpg',
+    imgAlt: 'my face'
+  }
+});
+
+const header = new Vue ({
+  el: '#header',
+  data: {
+    link: 'profile'
+  },
+  mounted: () => {
+    console.log('face is here');
+  }
+});
+
+function pxToNum(string) {
+  // remove "px" at the end and turn string to number
+  return Number(string.slice(0, string.length - 2));
+}
+
+// position face
+// elFaceImg.style.setProperty("--margin", `${compHeaderHeight}`);
+
+function placeFace() {
+  elFaceImg.style.marginTop = `${headerHeight*5}px`;
+  console.log('place face');
+}
+
+placeFace();
