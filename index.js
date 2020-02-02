e16f4dc5c74e9cbb21e8777c66eda9f2ea877362
emlie <mi-ya@live.no>
2020-02-02 23:23:58 +0100

    add socials; add currently

--- index.js
+++ index.js
@@ -5,23 +5,59 @@ const app = new Vue ({
   data: {
     showingProfile: false,
     showingProject: false,
-    esda: {
-      description: 'Assignments part of Elektronisk systemdesign og -analyse I & II (written in Norwegian).',
-      designNotes: [
-        {
-          title: 'Nivåregulator',
-          path: 'esda1/D1_Niv_regulator.pdf'
-        }
-      ],
-      appNotes: [
-        {
-          title: 'Turtallsindikator med transistor',
-          path: 'esda1/D1_Niv_regulator.pdf'
-        }
+    home: {
+      currently: [
+        'Vue.js',
+        'Backbone.js'
       ]
     },
+    profile: {
+      img: 'img/profile.jpg',
+      title: 'Mia Elisenberg',
+      text: 'Hey, this is some text about myself'
+    },
+    esda: {
+      title: 'ESDA',
+      description: 'Assignments part of Elektronisk systemdesign og -analyse I & II (written in Norwegian).',
+      designNotes: {
+        title: 'Design notes',
+        content: [
+          {
+            title: 'Nivåregulator',
+            path: 'esda1/D1_Niv_regulator.pdf'
+          }
+        ]
+      },
+      appNotes: {
+        title: 'Application notes',
+        content: [
+          {
+            title: 'Turtallsindikator med transistor',
+            path: 'esda1/D1_Niv_regulator.pdf'
+          }
+        ]
+      }
+    },
     other: {
+      title: 'Other',
       description: 'Some other projects',
-    }
+    },
+    socials: [
+      {
+        title: 'Github',
+        url: 'github.com/emlie/',
+        img: 'icons/github-image.svg'
+      },
+      {
+        title: 'Facebook',
+        url: 'https://www.facebook.com/mia.elisenberg',
+        img: 'icons/facebook.svg'
+      },
+      {
+        title: 'Instagram',
+        url: 'https://www.instagram.com/_emlie/',
+        img: 'icons/instagram.svg'
+      }
+    ]
   }
 });
