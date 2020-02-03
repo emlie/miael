abb0039c1a34b925ef4a0443cd54f57859e795d7
emlie <mi-ya@live.no>
2020-02-03 19:45:04 +0100

    fix vue router

--- index.js
+++ index.js
@@ -1,47 +1,146 @@
 // jshint esversion: 6
 
-const app = new Vue ({
-  el: '#app',
-  data: {
-    showingProfile: false,
-    showingProject: false,
-    home: {
-      currently: [
-        'Vue.js',
-        'Backbone.js'
-      ]
-    },
-    profile: {
-      img: 'img/profile.jpg',
-      title: 'Mia Elisenberg',
-      text: 'Hey, this is some text about myself'
-    },
-    esda: {
-      title: 'ESDA',
-      description: 'Assignments part of Elektronisk systemdesign og -analyse I & II (written in Norwegian).',
-      designNotes: {
-        title: 'Design notes',
-        content: [
-          {
-            title: 'Nivåregulator',
-            path: 'esda1/D1_Niv_regulator.pdf'
-          }
-        ]
-      },
-      appNotes: {
-        title: 'Application notes',
-        content: [
-          {
-            title: 'Turtallsindikator med transistor',
-            path: 'esda1/D1_Niv_regulator.pdf'
-          }
+// 1. Use plugin.
+// This installs <router-view> and <router-link>,
+// and injects $router and $route to all router-enabled child components
+Vue.use(VueRouter);
+
+const Profile = {
+  template: `
+  <div id="profile" class="content" v-bind="profile">
+    <h3>{{profile.title}}</h3>
+    <p>{{profile.text}}</p>
+  </div>
+  `,
+  data() {
+    return {
+      profile: {
+        img: 'img/profile.jpg',
+        title: 'Mia Elisenberg',
+        text: 'Hey, this is some text about myself'
+      }
+    }
+  }
+};
+
+const Home = {
+  template: `
+  <div id="home" class="content">
+    <h3>Currently working on</h3>
+    <ul>
+      <li v-for="thing in home.currently">{{thing}}</li>
+    </ul>
+  </div>
+  `,
+  data() {
+    return {
+      home: {
+        currently: [
+          'Vue.js',
+          'Backbone.js'
         ]
       }
-    },
-    other: {
-      title: 'Other',
-      description: 'Some other projects',
-    },
+    }
+  }
+};
+
+const Projects = {
+  template: `
+  <div id="projects" class="content">
+    <div class="project-collection">
+      <h3>{{esda.title}}</h3>
+      <p>{{esda.description}}</p>
+      <div class="projects">
+        <div>
+          <h2>{{esda.designNotes.title}}</h2>
+          <ul>
+            <li v-for="designNote in esda.designNotes.content">
+              <a :href="designNote.path">{{designNote.title}}</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>{{esda.appNotes.title}}</h2>
+          <ul>
+            <li v-for="appNote in esda.appNotes.content">
+              <a :href="appNote.path">{{appNote.title}}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="project-collection">
+      <h3>{{other.title}}</h3>
+      <p>{{other.description}}</p>
+      <div class="projects">
+        <div>
+          <h2>{{esda.designNotes.title}}</h2>
+          <ul>
+            <li v-for="designNote in esda.designNotes.content">
+              <a :href="designNote.path">{{designNote.title}}</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>{{esda.appNotes.title}}</h2>
+          <ul>
+            <li v-for="appNote in esda.appNotes.content">
+              <a :href="appNote.path">{{appNote.title}}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  `,
+  data() {
+    return {
+      esda: {
+        title: 'ESDA',
+        description: 'Assignments part of Elektronisk systemdesign og -analyse I & II (written in Norwegian).',
+        designNotes: {
+          title: 'Design notes',
+          content: [
+            {
+              title: 'Nivåregulator',
+              path: 'esda1/D1_Niv_regulator.pdf'
+            }
+          ]
+        },
+        appNotes: {
+          title: 'Application notes',
+          content: [
+            {
+              title: 'Turtallsindikator med transistor',
+              path: 'esda1/D1_Niv_regulator.pdf'
+            }
+          ]
+        }
+      },
+      other: {
+        title: 'Other',
+        description: 'Some other projects',
+      }
+    }
+  }
+};
+
+const router = new VueRouter ({
+  routes: [
+    { path: '/profile', component: Profile },
+    { path: '/', component: Home },
+    { path: '/projects', component: Projects }
+  ]
+});
+
+const app = new Vue ({
+  el: '#app',
+  router,
+  data: {
+    /*
+    showingProfile: false,
+    showingProject: false,
+    */
     socials: [
       {
         title: 'Github',
@@ -60,4 +159,4 @@ const app = new Vue ({
       }
     ]
   }
-});
+}).$mount('#app');
